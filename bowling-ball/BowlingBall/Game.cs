88a459eacecd859ed18a88d5951556ffd06111d6
Swati409 <68477967+Swati409@users.noreply.github.com>
2022-09-06 22:38:06 +0530

    Update Game.cs
    Added code for Bowling game

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +8,47 @@ namespace BowlingBall
 {
     public class Game
     {
+       //public List<int> rolls = new List<int>();
+        private int[] rolls = new int[20];
+        private int currentroll = 0;
+
+        public int score {
+            get{
+                var score = 0;
+                var rindex = 0;
+                for(var frames = 0; frame <10; frame++){
+                    if(rolls[rindex] + rolls[rindex + 1] == 10) // strike
+                    {
+                         score += rolls[rindex] + rolls[rindex+1] + rolls[rindex+2];   
+                         rindex++;
+                    }
+                    else if (rolls[index ==10]){ //spare
+                        score += rolls[rindex] + rolls[rindex+1] + rolls[rindex+2];
+                        rindex += 2;
+                    }
+                    else{//standard score
+                       score += rolls[rindex] + rolls [rindex+1];  
+                       rindex += 2;
+                    }
+                    
+                    
+                }
+             
+                return score;       
+            }
+         }
         public void Roll(int pins)
         {
+            rolls[currentroll++] = pins;
             // Add your logic here. Add classes as needed.
         }
 
         public int GetScore()
         {
             // Returns the final score of the game.
-            return 0;
+            return score;
         }
+
+        
     }
 }
